c4b61110b972bbe7561f2a269362659fbc966cf1
aryan <aryan.shir07@gmail.com>
2025-12-25 16:14:28 +0530

    Implement BFS algorithm in C

---
+++ Lab prgm 9/bsf.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+
+void bfs(int v);
+
+int a[10][10], vis[10], n;
+
+int main()  
+{
+    int i, j, src;
+    
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+    
+    printf("Enter the adjacency matrix:\n");
+    for(i = 1; i <= n; i++)
+    {
+        for(j = 1; j <= n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+        vis[i] = 0;  // Initialize all vertices as unvisited
+    }
+    
+    printf("Enter the source vertex: ");
+    scanf("%d", &src);
+    
+    printf("Nodes reachable from source vertex %d: ", src);
+    bfs(src);
+    
+    printf("\n");
+    return 0;
+}
+
+void bfs(int v)
+{
+    int q[10], f = 1, r = 1, u, i;
+    
+    q[r] = v;     
+    vis[v] = 1;   
+    
+    while(f <= r) 
+    {
+        u = q[f]; 
+        printf("%d ", u);
+        
+       
+        for(i = 1; i <= n; i++)
+        {
+            if(a[u][i] == 1 && vis[i] == 0)  
+            {
+                vis[i] = 1;    
+                r = r + 1;      
+                q[r] = i;       
+            }
+        }
+        f = f + 1;  
+    }
+}
